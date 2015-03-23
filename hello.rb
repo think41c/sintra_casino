4881c60b93880a0557a712888a657a027ca558c1
Tony DiNitto <think41c@gmail.com>
2015-03-22 18:05:04 -0600

    Different route handlers introduced.

--- hello.rb
+++ hello.rb
@@ -1,5 +1,10 @@
-require 'sinatra'
-get '/hello' do
-  name = "port"
+require 'sinatra' 
+require 'sinatra/reloader'# if_development?
+get '/:name' do
+  name = params[:name]
   "Helloooo! #{name}"
+end
+
+get '/:a/:b/:c' do
+ "first: #{params[:a]}, second: #{params[:b]}, third: #{params[:c]}"
 end
